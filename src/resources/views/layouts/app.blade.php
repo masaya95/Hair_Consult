bfaadb6abf4a324ef99bfa5d6436b318d6ccba31
masaya_onami <masaya.onami@gmail.com>
2020-10-10 21:33:21 +0900

    google analytics

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -24,6 +24,16 @@
 
     <!-- viewport meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180265147-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-180265147-1');
+    </script>
 </head>
 <body>
   <div id="app">
