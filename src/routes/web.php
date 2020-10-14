003ccd92c60ed48a39c6420fcbf17bc57be1be47
masaya_onami <masaya.onami@gmail.com>
2020-10-14 20:23:21 +0900

    Auth:routes()コメントアウト

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostController@index');
 
-Auth::routes();
+// Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
   Route::get('/post/article', 'PostController@article');
